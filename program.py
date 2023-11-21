581c12b43b264828aa01bd2fa034bf269a0b8d15
flamingoo111 <151651393+flamingoo111@users.noreply.github.com>
2023-11-21 18:03:02 +0300

    Update program.py
    new print just yeee

--- program.py
+++ program.py
@@ -2,7 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('More priiiints')
-
+    print('UFO came and added this line')
 
 if __name__ == '__main__':
     main()
